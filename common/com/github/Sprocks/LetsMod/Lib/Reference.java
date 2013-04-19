6526b3ffa86011b3ba22fb6e86c502f7eef1b43f
Sprocks59 <Ryan@10.37.129.2>
2013-04-19 00:13:05 -0400

    Added mcmod info, main class, reference class

---
+++ common/com/github/Sprocks/LetsMod/Lib/Reference.java
@@ -0,0 +1,8 @@
+package com.github.Sprocks.LetsMod.Lib;
+
+public class Reference
+{
+    public static final String MOD_ID = "LetsMod";
+    public static final String MOD_NAME = "Lets Mod";
+    public static final String MOD_VERSION = "0.0.1";
+}
